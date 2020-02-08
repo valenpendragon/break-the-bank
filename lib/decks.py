b2b5236474769bbc525a2ed444a2c5ff4b56f216
Jeffrey Scott <valenpendragon@gmail.com>
2020-02-08 16:08:44 -0500

    2020-02-08 14:xx: For class AbstractCard, completed __init__(), __str__(), and abstractmethod __repr__(). For classes Ace, FaceCard, and NumberCard, completed __init__(), __str__(), and __repr__(). Fully tested each class. No diagnostic printouts alternates for __str__() functions at this time.

--- lib/decks.py
+++ lib/decks.py
@@ -20,13 +20,13 @@ classes in this library:
 
     Class AbstractCard: Abstract card class
         SubClass Ace: Store aces and their second value
-        SubClass Face: Stores face cards with their constant value of 10
-        SubClass Card: Stores cards with numberical values on the face that
-            match the card's score value 
+        SubClass FaceCard: Stores face cards with their constant value of 10
+        SubClass NumberCard: Stores cards with numberical values on the face
+            that match the card's score value 
     
     Class Deck: Object containing 52 cards, randomly shuffled with extra
         entropy
-        SubClass CardShoe: A multideck (2-8 decks) also fully shuffled
+        SubClass CardShoe: A multideck shoe (2-8 decks) also fully shuffled
             with extra entropy.
 
 Both classes require Abstract Base Class. The Deck class requires the
@@ -36,20 +36,17 @@ random package in addition.
 import random as rd
 from abc import ABC, abstractmethod, abstractstaticmethod
 
-# Constants:
-RANKS = ('A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K')
-SUITS = ('S', 'D', 'H', 'C')
-
 class AbstractCard(ABC):
-    '''
+    """
     This class sets up how cards are initialized.
 
     Methods:
-        __init__: puts the rank and suit into the correct attributes, value
+        __init__: Puts the rank and suit into the correct attributes, value
             is handled in the subclasses. Valid rank and suit checkes are
-            made and will raise ValueError if incorrect. Used by super().
-        __str__ : abstractmethod, overridden in subclasses
-        __repr__: abstractmethod, overridden in subclasses
+            made and will raise ValueError if incorrect. Subclasses should
+            invoke this wits super().
+        __str__ : returns string 'rank-suit'. Subclasses invoke with super().
+        __repr__: abstractmethod only
 
     Attributes:
         self.rank: This is the rank of the card. Valid values are found in
@@ -58,6 +55,144 @@ class AbstractCard(ABC):
             represented by the first character of the name of the suit.
             Valid values are in SUITS constant.
         self.value: Initialized by subclasses only.
-    '''
+    """
+    # Constants:
+    RANKS = ('A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K')
+    SUITS = ('S', 'D', 'H', 'C')
+    FACECARDS = ('J', 'Q', 'K')
+    NUMBERCARDS = ('2', '3', '4', '5', '6', '7', '8', '9', '10')
 
- 
+    # Methods
+    def __init__(self, rank, suit):
+        """
+        Usage: AbstractCard(rank, suit). This method will validate the rank
+        and suit against constants RANKS and SUITS from the class. The
+        card's value as part of a hand is left to subclasses. Invoke this
+        abstract base class method via super() method.
+        INPUTS: two strings, rank and suit
+        OUTPUTS: none outside of creating the card object
+        """
+        if rank not in self.RANKS:
+            print(f"AbstractCard: An invalid rank was supplied {rank}.")
+            raise ValueError("Card objects must have a valid rank.") from None
+            return None
+
+        if suit not in self.SUITS:
+            print(f"AbstractCard: An invalid suit was supplied {suit}.")
+            raise ValueError("Card objects must have a valid suit.") from None
+            return None
+        self.rank = rank
+        self.suit = suit
+        self.value = None
+
+    def __str__(self):
+        """
+        Returns a three character string '{rank}-{suit}'
+        """
+        return "{rank}-{suit} ".format(rank=self.rank, suit=self.suit)
+
+    @abstractmethod
+    def __repr__(self):
+        pass
+
+class Ace(AbstractCard):
+    """
+    Usage: Ace(suit). This class uses the methods from AbstractCard and
+    adds an extra attribute, high_value, to this object. It only requires
+    a suit.
+    """
+
+    # Methods
+    def __init__(self, suit):
+        """
+        Usage: Ace(suit). This method calls super().__init__() to run
+        checks on the suit and set the rank and suit.
+        INPUT: one string, suit
+        OUTPUTS: none
+        """
+        super().__init__(rank='A', suit=suit)
+        self.value = 1
+        self.high_value = 11
+    
+    def __str__(self):
+        """
+        Returns a three character string 'A-{suit}'.
+        """
+        output = super().__str__()
+        return output
+
+    def __repr__(self):
+        """
+        Returns the class call.
+        """
+        return 'Ace({suit})'.format(suit=self.suit)
+
+class FaceCard(AbstractCard):
+    """
+    Usage: FaceCard(rank, suit). This class uses the methods from AbstractCard
+    without adding any additional attributes.
+    """
+    # All face cards have a value of 10 in Blackjack.
+    CARDVALUE = 10
+
+    # Methods
+    def __init__(self, rank, suit):
+        """
+        Usage: FaceCard(rank, suit). This method calls super().__init__() to
+        run checks on the suit and set the rank and suit. It then sets the
+        value attribute.
+        """
+        if rank not in self.FACECARDS:
+            print(f"FaceCard: An invalid rank was supplied {rank}.")
+            raise ValueError("FaceCard.rank must be J, Q, or K") from None
+            return None
+        super().__init__(rank=rank, suit=suit)
+        self.value = self.CARDVALUE
+    
+    def __str__(self):
+        """
+        Returns a three character string '{rank}-{suit}'.
+        """
+        output = super().__str__()
+        return output
+
+    def __repr__(self):
+        """
+        Returns the class call.
+        """
+        return 'FaceCard({rank}, {suit})'.format(rank=self.rank, 
+                                                   suit=self.suit)
+
+class NumberCard(AbstractCard):
+    """
+    Usage: NumberCard(rank, suit). This class uses the methods from
+    AbstractCard without adding any additional attributes.
+    """
+    
+    # Methods
+    def __init__(self, rank, suit):
+        """
+        Usage: NumberCard(rank, suit). This method calls super().__init__() to
+        run checks on the suit and set the rank and suit. It then sets the
+        value attribute.
+        """
+        if rank not in self.NUMBERCARDS:
+            print(f"NumberCard: An invalid rank was supplied {rank}.")
+            raise ValueError("NumberCard.rank must be an integer") from None
+            return None
+        super().__init__(rank=rank, suit=suit)
+        self.value = int(self.rank)
+    
+    def __str__(self):
+        """
+        Returns a three character string '{rank}-{suit}'.
+        """
+        output = super().__str__()
+        return output
+
+    def __repr__(self):
+        """
+        Returns the class call.
+        """
+        return 'NumberCard({rank}, {suit})'.format(rank=self.rank, 
+                                                   suit=self.suit)
